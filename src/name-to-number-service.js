b4098bafd881e2665e922a3fb5fe38d6f0fdcf53
Matt Hilton <matt@mjhilton.net>
2017-11-17 10:05:15 +1100

    Added more tests for name-to-number service and refactored code

--- src/name-to-number-service.js
+++ src/name-to-number-service.js
@@ -13,42 +13,16 @@ const alphabet = ['a', 'b', 'c', 'd', 'e','f','g','h','i','j','k','l','m','n','o
 //
 // Therefore generateNumber('Aaron', 4) returns 1; generateNumber('Matt', 4) returns 3.
 function generateNumber(name, numberOfBuckets) {
-    assert.ok(numberOfBuckets <= lettersInAlphabet, 'numberOfBuckets must be equal to or less than 26');
+    assert.ok(numberOfBuckets < lettersInAlphabet, 'numberOfBuckets must be equal to or less than 26');
     assert.ok(typeof(name) === 'string', 'name must be a string');
     assert.ok(name.length > 0, 'name must have at least 1 letter');
 
-    var buckets = bucketiseAlphabet(numberOfBuckets);
-    var searchForLetter = name[0].toLowerCase();
-    
-    var foundInBucket = null;
-    for (var i = 0; i < buckets.length; i++) {
-        if (buckets[i].indexOf(searchForLetter) !== -1)
-            foundInBucket = i + 1;
-    }
+    var bucketSize = Math.floor(lettersInAlphabet / numberOfBuckets);
+    var firstLetterInName = name[0].toLowerCase();
+    var firstLetterLocation = alphabet.indexOf(firstLetterInName) + 1;
+    var firstLetterBucket = Math.min(numberOfBuckets, Math.ceil(firstLetterLocation / bucketSize));
 
-    return foundInBucket;
-};
-
-function bucketiseAlphabet(numberOfBuckets) {
-    var numberOfLettersInBucket = Math.floor(lettersInAlphabet / numberOfBuckets);
-    var buckets = []
-    
-    // Split the alphabet up into buckets
-    for (var currentBucket = 1; currentBucket <= numberOfBuckets; currentBucket++) {
-        var alphabetStartIndex = (currentBucket - 1) * numberOfLettersInBucket;
-
-        buckets[currentBucket - 1] = alphabet.slice(alphabetStartIndex, alphabetStartIndex + numberOfLettersInBucket);
-    }
-
-    // If there's any letters left at the end, add them to the last bucket
-    if (lettersInAlphabet % numberOfBuckets !== 0) {
-        var leftoverIndex = numberOfLettersInBucket * numberOfBuckets;
-        var lastBucket = buckets[numberOfBuckets - 1];
-
-        buckets[numberOfBuckets - 1] = lastBucket.concat(alphabet.slice(leftoverIndex, lettersInAlphabet));
-    }
-
-    return buckets;
+    return firstLetterBucket;
 };
 
 module.exports = {
--- test/name-to-number-service-tests.js
+++ test/name-to-number-service-tests.js
@@ -3,15 +3,57 @@ var chai = require('chai');
 chai.should();
 
 describe("Name to number service", function() {
+    describe("with 1 bucket", function() {
+        var getBucket = (name) => nameToNumberService.generateNumber(name, 10);
+
+        it("puts Jenny in the first", function() {
+            getBucket("Jenny").should.equal(1);
+        })
+    });
+
     describe("with 4 buckets", function() {
+        var getBucket = (name) => nameToNumberService.generateNumber(name, 4);
+
         it("puts Alice in the first", function() {
-            var buckets = 4;
-            var name = "Alice";
-            var expectedBucket = 1;
+            getBucket("Alice").should.equal(1);
+        });
 
-            var actualBucket = nameToNumberService.generateNumber(name, buckets);
+        it("puts John in the second", function() {
+            getBucket("John").should.equal(2);
+        });
 
-            actualBucket.should.equal(expectedBucket);
+        it("puts Olli in the third", function() {
+            getBucket("Olli").should.equal(3);
+        });
+
+        it("puts Zara in the fourth", function() {
+            getBucket("Zara").should.equal(4);
+        });
+    });
+
+    describe("with 26 buckets", function() {
+        var getBucket = (name) => nameToNumberService.generateNumber(name, 26);
+
+        it("puts Alice in the first", function() {
+            getBucket("Alice").should.equal(1);
+        });
+
+        it("puts Bryce in the second", function() {
+            getBucket("Bryce").should.equal(2);
+        });
+
+        it("puts Yolanie in the twenty fifth", function() {
+            getBucket("Yolanie").should.equal(25);
+        });
+
+        it("puts Zara in the twenty sixth", function() {
+            getBucket("Zara").should.equal(26);
+        });
+    });
+
+    describe("with 27 buckets", function() {
+        it("fails", function() {
+            chai.expect(() => nameToNumberService.generateNumber("Alice", 27)).to.throw(/less than 26/);
         });
     });
 });
